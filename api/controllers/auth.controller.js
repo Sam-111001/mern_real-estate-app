919aff281dcd7fa24e53ec2754e57f70cf8c6e89
Shamant Myageri <code.shamant@gmail.com>
2025-02-01 19:51:54 +0530

    Added a middlewear to handle the error, also created a function to throw personalized error.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 //Function that handles the signup request
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body; //Object destructuring
 
   const hassedPassword = bcryptjs.hashSync(password, 10); //Encrypting the password
@@ -13,6 +14,6 @@ export const signup = async (req, res) => {
     await newUser.save(); //updating the database
     res.status(201).json("Successfully added new user!");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
